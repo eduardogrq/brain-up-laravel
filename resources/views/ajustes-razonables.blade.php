70173031fdf4639ad7778b86ccf73eb4036614b0
Eduardo guadalupe Romero quiñonez <eduardogrq@Eduardos-iMac.local>
2021-08-11 12:28:53 -0500

    Se agregan analytics a todas las vistas.

--- resources/views/ajustes-razonables.blade.php
+++ resources/views/ajustes-razonables.blade.php
@@ -9,6 +9,17 @@
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="css/ajustes.css">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-F90XM3LSXJ"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-F90XM3LSXJ');
+    </script>
+
 </head>
 <body>
 
--- resources/views/recursos/layouts/master.blade.php
+++ resources/views/recursos/layouts/master.blade.php
@@ -16,6 +16,16 @@
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     <link rel="stylesheet" href="./../../../css/style.css">
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-F90XM3LSXJ"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-F90XM3LSXJ');
+    </script>
+
     <title>Inicio</title>
 </head>
 <body>
